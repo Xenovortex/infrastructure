83617dbd5dc8459623a8c1644ff58389c15f8caa
Lu Liu <nudtlliu@gmail.com>
2017-04-26 11:46:08 +0200

    write the key to a file

--- tyk/acquire_apikey.py
+++ tyk/acquire_apikey.py
@@ -3,7 +3,8 @@
 author: Lu Liu
 created at: 2017-04-26
 Description:
-    Get a new API key for the openrouteservice public web client
+    Get a new API key for the openrouteservice public web client, then save it
+    to a plain text file `ors_apikey.txt` in the current working dir
 """
 
 import json
@@ -14,6 +15,7 @@ except ImportError:
     from urllib.parse import urljoin
 
 openrouteservice_org_id = "58d904a497c67e00015b45fc"
+tyk_auth_token = "36010471e7c6aa3e0d91104ceb09119f"
 tyk_token_session = {
     "last_check": 0,
     "allowance": 1000,
@@ -30,7 +32,7 @@ tyk_token_session = {
 tyk_gateway_addr = "http://192.168.2.17:8080"
 apikey_mgt_url = "/tyk/keys/create"
 tyk_admin_headers = {
-    "x-tyk-authorization": "fb537f41eef94b4c615a1b6414ae0920",
+    "x-tyk-authorization": tyk_auth_token,
     "Cache-Control": "no-cache"
 }
 
@@ -39,4 +41,15 @@ resp = requests.post(
     data=json.dumps(tyk_token_session),
     headers=tyk_admin_headers)
 
-print(resp)
+key_file_name = "ors_apikey.txt"
+if resp.status_code == 200:
+    print("Successfully got a new api key for openrouteservice.org")
+    json_response = resp.json()
+    print("raw response: " + json_response)
+    with open(key_file_name, 'w') as key_file:
+        key_file.write(json_response["key"])
+    print("Wrote the new API key to ors_apikey.txt file")
+    print("The key will be expired in 24 hours")
+else:
+    print("Failed to fetch the new key.")
+    print("raw response: " + resp.json())
