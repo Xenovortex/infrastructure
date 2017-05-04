7586c18e9c48fc53a70365f1ddc52a5d8f37dffa
Lu Liu <nudtlliu@gmail.com>
2017-05-04 13:51:17 +0000

    use external storage to incrementally record the web log stats

--- goaccess/start_goaccess_daemon.sh
+++ goaccess/start_goaccess_daemon.sh
@@ -2,7 +2,9 @@
 goaccess -f /var/log/nginx/access.log \
 	 -o /var/www/api.openrouteservice.org/html/report.html \
 	 --real-time-html \
-	 --daemonize \
 	 --ssl-cert=/etc/letsencrypt/live/api.openrouteservice.org/fullchain.pem \
 	 --ssl-key=/etc/letsencrypt/live/api.openrouteservice.org/privkey.pem \
-         --ws-url=wss://api.openrouteservice.org:7890
+         --ws-url=wss://api.openrouteservice.org:7890 \
+         --load-from-disk \
+         --keep-db-files \
+	 --daemonize
