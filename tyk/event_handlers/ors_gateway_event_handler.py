a64911f5cece1b55804dc39cbbbee22a4b27a38e
sentinel <ubuntu@openrouteservice.org>
2017-05-19 12:20:43 +0000

    add str() to password for using in python2; add logging; add other stuff for uwsgi

--- tyk/event_handlers/ors_gateway_event_handler.py
+++ tyk/event_handlers/ors_gateway_event_handler.py
@@ -1,8 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+import logging.config
 import json
+
+LOGGING_CONF_FILE = 'logging.json'
+DEFAULT_LOGGING_LVL = logging.INFO
+path = LOGGING_CONF_FILE
+value = os.getenv('LOG_CFG', None)
+if value:
+    path = value
+if os.path.exists(path):
+    with open(path, 'rt') as f:
+       config = json.load(f)
+    logging.config.dictConfig(config)
+else:
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+from flask import Flask
+
+PROJECT_DIR, PROJECT_MODULE_NAME = os.path.split(
+    os.path.dirname(os.path.realpath(__file__))
+)
+
+ORS_GATEWAY_EVENT_HANDLER_DIR = os.path.join(PROJECT_DIR, os.pardir)
+if os.path.exists(ORS_GATEWAY_EVENT_HANDLER_DIR) \
+        and ORS_GATEWAY_EVENT_HANDLER_DIR not in sys.path:
+    sys.path.append(ORS_GATEWAY_EVENT_HANDLER_DIR)
+
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from flask import Flask
 from flask import request
 
 app = Flask(__name__)
@@ -20,13 +52,14 @@ def gateway_events_handler():
                       contacts['sender']['email'],
                       contacts['sender']['password'], r['email'], msg_subject,
                       json.dumps(event_body))
-    return "Unauthorised request"
+        return "Alert e-mails have been sent"
+    return ("Unauthorised request", 401, {})
 
 
 def send_mail(smtp_host, smtp_port, from_addr, password, to_addr, msg_subject,
               msg_text):
     s = smtplib.SMTP_SSL(smtp_host)
-    s.login(from_addr, password)
+    s.login(from_addr, str(password))
 
     mime_msg = MIMEMultipart()  # create a message
     mime_msg['From'] = from_addr
@@ -36,3 +69,6 @@ def send_mail(smtp_host, smtp_port, from_addr, password, to_addr, msg_subject,
 
     s.sendmail(from_addr, to_addr, mime_msg.as_string())
     s.quit()
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', debug=True)
