c6883a0d5038ad3e238cb48f276181aa67b1fea2
Lu Liu <nudtlliu@gmail.com>
2017-08-15 08:50:48 +0200

    get ors registered developers via tyk dashboard API, page by page

---
+++ tyk/fetch_ors_developers.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+START_PAGE=${1-0}
+END_PAGE=${2-1}
+for i in $(seq $START_PAGE $END_PAGE)
+do
+    echo "Fetching page $i of ORS developers..."
+    curl --header "authorization: fb537f41eef94b4c615a1b6414ae0920" https://admin.cloud.tyk.io/api/portal/developers?p=$i | python -m json.tool > ors_developers_p$i.json
+    echo "done."
+done
