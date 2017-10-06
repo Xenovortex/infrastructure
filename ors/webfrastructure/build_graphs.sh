ed293d417b7765d706966920acfe50b2a4ec8ea5
Timothy Ellersiek <merusame@gmail.com>
2017-10-06 14:28:47 +0200

    graph update script checks md5sum

--- ors/webfrastructure/build_graphs.sh
+++ ors/webfrastructure/build_graphs.sh
@@ -7,14 +7,33 @@ echo "/ \_//| |_) |  __/ | | / _  \ (_) | |_| | ||  __/\ \  __/ |   \ V /| | (_|
 echo "\___/ | .__/ \___|_| |_\/ \_/\___/ \__,_|\__\___\__/\___|_|    \_/ |_|\___\___| "
 echo "      |_|                                                                       "
 
-ORS_ROOT=/opt/openrouteservice/webfrastructure
+ORS_ROOT="$PWD"
+ORS_TOMCAT_DATA_DIR="/tomcat/data"
+ORS_OSM_DATA_DIR="/hayloft/osm"
 
 Compute_graphs () {
     echo "==> Graphs are built, starting to update graphs"
-    cp -R $ORS_ROOT/tomcat/data/graphs $ORS_ROOT/tomcat/data/graphs_latest/
-    wget -P $ORS_ROOT/tomcat/data/osm/ -q https://planet.osm.org/pbf/planet-latest.osm.pbf -O planet-latest.osm.pbf
-    rm -rf $ORS_ROOT/tomcat/data/graphs/*
-    docker restart ors-app
+
+    if [ -d ${ORS_ROOT}${ORS_TOMCAT_DATA_DIR}/latest ]; then
+       rm -r ${ORS_ROOT}${ORS_TOMCAT_DATA_DIR}/latest/*
+    fi
+
+    rm -r ${ORS_ROOT}${ORS_TOMCAT_DATA_DIR}/latest/*
+
+    DATE_NOW=$(date +%Y%m%d_%H%M%S)
+    LATEST_DIR="${ORS_ROOT}${ORS_TOMCAT_DATA_DIR}/latest/graphs.${DATE_NOW}"
+    mkdir -p $LATEST_DIR
+    cp -R ${ORS_ROOT}${ORS_TOMCAT_DATA_DIR}/graphs/* $LATEST_DIR
+
+    MD5SUM_CLOUD=$(curl https://planet.osm.org/pbf/planet-latest.osm.pbf.md5 | head -n 1 | cut -c -32)
+    wget -q -O ${ORS_ROOT}${ORS_OSM_DATA_DIR}/planet-latest.osm.pbf https://planet.osm.org/pbf/planet-latest.osm.pbf
+    MD5SUM_LOCAL=$(md5sum ${ORS_ROOT}${ORS_OSM_DATA_DIR}/planet-latest.osm.pbf | head -n 1 | cut -c -32)
+    if [ "$MD5SUM_CLOUD" == "$MD5SUM_LOCAL" ]; then
+        rm -rf ${ORS_ROOT}${ORS_TOMCAT_DATA_DIR}/graphs/*
+        docker restart ors-app
+    else
+        Compute_graphs
+    fi
 }
 
 echo "==> Checking if graphs can be updated"
