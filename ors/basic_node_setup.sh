e4b8733c1073eff3ccd427ccbad73faec81436f3
timothy <ellersiek@uni-heidelberg.de>
2017-10-21 11:51:44 +0000

    Add new file

---
+++ ors/basic_node_setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Add google nameservers
+echo "dns-nameservers 8.8.8.8 8.8.4.4" >> /etc/network/interfaces.d/50-cloud-init.cfg
+ifdown ens3 && ifup ens3
+
+# Add hostname
+sed -i "1s/.*/127.0.0.1 localhost $HOSTNAME/" /etc/hosts
+
+# Install docker
+apt-get update
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install -y docker-ce
+
+# Install docker-compose
+chmod -R 777 /usr/local/bin
+curl -L https://github.com/docker/compose/releases/download/1.11.2/docker-compose-Linux-x86_64 -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+exit 0
