c2c4a5780028e0e47af2e4d15d92fd1e6837c541
timothy <ellersiek@uni-heidelberg.de>
2017-11-05 19:37:49 +0000

    Update cloud-init.sh

--- openstack/cloud-init.sh
+++ openstack/cloud-init.sh
@@ -30,4 +30,10 @@ chmod -R 777 /usr/local/bin
 curl -L https://github.com/docker/compose/releases/download/1.11.2/docker-compose-Linux-x86_64 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
+# Install pip
+apt-add-repository ppa:ansible/ansible
+apt-get update
+apt-get install -y python-pip \
+    ansible
+
 exit 0
