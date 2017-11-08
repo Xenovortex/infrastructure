d3b966964e11d3738f66194d41ece858044eb9e8
Lu Liu <nudtlliu@gmail.com>
2017-11-08 12:59:58 +0000

    Update tyk gateway bootstrap scripts

---
+++ tyk/bootstrap-tyk.sh
@@ -0,0 +1,2 @@
+# The last two params REDIS_HOST and REDIS_PORT can be omitted because the values in the tyk config file have higher priorities
+sudo ./start-bundle-python.sh 8080 36010471e7c6aa3e0d91104ceb09119f 58d904a497c67e00015b45fc fb537f41eef94b4c615a1b6414ae0920 192.168.2.27 6379
