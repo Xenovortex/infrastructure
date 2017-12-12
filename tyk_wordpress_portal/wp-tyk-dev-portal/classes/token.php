52e13c7a570b95db61467f7b53a97d2a082758fb
Timothy Ellersiek <merusame@gmail.com>
2017-12-12 17:24:47 +0100

    changes

--- tyk_wordpress_portal/wp-tyk-dev-portal/classes/token.php
+++ tyk_wordpress_portal/wp-tyk-dev-portal/classes/token.php
@@ -58,11 +58,12 @@ class Tyk_Token
      * @param Portal_User $user
      * @param string $policy optional, not required for all actions
      */
-    public function __construct(Tyk_Portal_User $user, $policy = null) {
-        $this->api = new Tyk_API;
+    public function __construct(Tyk_Portal_User $user, $policy = null)
+    {
+        $this->api     = new Tyk_API;
         $this->gateway = new Tyk_Gateway;
-        $this->user = $user;
-        //PC::debug($this);
+        $this->user    = $user;
+        ////PC::debug($this);
         if (!is_null($policy)) {
             $this->policy = $policy;
         }
@@ -75,16 +76,16 @@ class Tyk_Token
      * @param Portal_User $user
      * @return Tyk_Token
      */
-    public static function init(array $token, Tyk_Portal_User $user) {
+    public static function init(array $token, Tyk_Portal_User $user)
+    {
         if (isset($token['api_id']) && isset($token['hash'])) {
             $instance = new Tyk_Token($user, $token['api_id']);
             $instance->set_hash($token['hash']);
             $instance->set_name($token['token_name']);
             $instance->set_key($token['key']);
-            //PC::debug($instance);
+            ////PC::debug($instance);
             return $instance;
-        }
-        else {
+        } else {
             throw new InvalidArgumentException('Invalid token specified');
         }
     }
@@ -96,18 +97,20 @@ class Tyk_Token
      * @param Tyk_Portal_User $user
      * @return Tyk_Token
      */
-    public static function init_from_key($key, Tyk_Portal_User $user) {
+    public static function init_from_key($key, Tyk_Portal_User $user)
+    {
         $token = new Tyk_Token($user);
         $token->set_key($key);
         return $token;
     }
 
-    public static function var_error_log( $object=null ){
-    	ob_start();                    // start buffer capture
-    	var_dump( $object );           // dump the values
-    	$contents = ob_get_contents(); // put the buffer into a variable
-    	ob_end_clean();                // end capture
-    	error_log( $contents );        // log contents of the result of var_dump( $object )
+    public static function var_error_log($object = null)
+    {
+        ob_start(); // start buffer capture
+        var_dump($object); // dump the values
+        $contents = ob_get_contents(); // put the buffer into a variable
+        ob_end_clean(); // end capture
+        error_log($contents); // log contents of the result of var_dump( $object )
     }
 
     /**
@@ -115,7 +118,8 @@ class Tyk_Token
      *
      * @return string
      */
-    public function get_id() {
+    public function get_id()
+    {
         return $this->id;
     }
 
@@ -125,7 +129,8 @@ class Tyk_Token
      *
      * @return string
      */
-    public function set_id($id) {
+    public function set_id($id)
+    {
         $this->id = $id;
     }
 
@@ -134,7 +139,8 @@ class Tyk_Token
      *
      * @return string
      */
-    public function get_key() {
+    public function get_key()
+    {
         return $this->key;
     }
 
@@ -143,7 +149,8 @@ class Tyk_Token
      *
      * @param string $key
      */
-    public function set_key($key) {
+    public function set_key($key)
+    {
         $this->key = $key;
     }
 
@@ -152,8 +159,9 @@ class Tyk_Token
      *
      * @param string $hash
      */
-    public function set_hash($hash) {
-        PC::debug($this->hash);
+    public function set_hash($hash)
+    {
+        //PC::debug($this->hash);
         $this->hash = $hash;
     }
 
@@ -162,7 +170,8 @@ class Tyk_Token
      *
      * @return string
      */
-    public function get_hash() {
+    public function get_hash()
+    {
         return $this->hash;
     }
 
@@ -171,7 +180,8 @@ class Tyk_Token
      *
      * @param string $name
      */
-    public function set_name($name) {
+    public function set_name($name)
+    {
         $this->name = $name;
     }
 
@@ -181,7 +191,8 @@ class Tyk_Token
      *
      * @return string
      */
-    public function get_name() {
+    public function get_name()
+    {
         return $this->name;
     }
 
@@ -190,7 +201,8 @@ class Tyk_Token
      *
      * @return string
      */
-    public function get_policy() {
+    public function get_policy()
+    {
         return $this->policy;
     }
 
@@ -201,19 +213,20 @@ class Tyk_Token
      *
      * @return string
      */
-    public function request() {
+    public function request()
+    {
         $request_id = $this->api->post('/portal/requests', array(
-            'by_user' => $this->user->get_tyk_id(),
-            'for_plan' => $this->policy,
+            'by_user'      => $this->user->get_tyk_id(),
+            'for_plan'     => $this->policy,
             // it's possible to have key requests approved manually
-            'approved' => TYK_AUTO_APPROVE_KEY_REQUESTS,
+            'approved'     => TYK_AUTO_APPROVE_KEY_REQUESTS,
             // this is a bit absurd but tyk api doesn't set this by itself
             'date_created' => date('c'),
-            'version' => 'v2',
+            'version'      => 'v2',
         ));
-        // PC::debug($request_id);
-        // PC::debug($this);
-        // PC::debug(array(
+        // //PC::debug($request_id);
+        // //PC::debug($this);
+        // //PC::debug(array(
         //     'by_user' => $this->user->get_tyk_id(),
         //     'for_plan' => $this->policy,
         //     // it's possible to have key requests approved manually
@@ -226,8 +239,7 @@ class Tyk_Token
         // save key request id
         if (is_string($request_id)) {
             $this->id = $request_id;
-        }
-        else {
+        } else {
             throw new UnexpectedValueException('Received an invalid response for key request');
         }
     }
@@ -244,13 +256,14 @@ class Tyk_Token
      *
      * @return void
      */
-    public function approve() {
+    public function approve()
+    {
         if (!is_string($this->id) || empty($this->id)) {
             throw new InvalidArgumentException('Invalid key request');
         }
 
         try {
-            $token = $this->api->put('/portal/requests/approve', $this->id);
+            $token     = $this->api->put('/portal/requests/approve', $this->id);
             $developer = $this->user->fetch_from_tyk();
             if (is_object($token) && isset($token->RawKey) && !empty($token->RawKey)) {
                 $this->key = $token->RawKey;
@@ -260,83 +273,56 @@ class Tyk_Token
                         $this->hash = $developer->subscriptions->{$this->policy};
                     }
                 }
-            }
-            else {
+            } else {
                 throw new Exception('Could not approve token request');
             }
-        }
-        catch (Exception $e) {
+        } catch (Exception $e) {
             throw new UnexpectedValueException($e->getMessage());
         }
     }
-
-
-
 
     /**
      * Revoke (delete!) a token
      *
      * @throws InvalidArgumentException When this class doesn't have all the data it needs
      * @throws UnexpectedValueException When API does not respond as expected
-     * 
+     *
      * @return boolean True when successful
      */
-    public function revoke() {
-        
-        PC::debug($this);
+    public function revoke()
+    {
 
         if (!is_string($this->policy) || !is_string($this->hash) || !is_a($this->user, 'Tyk_Portal_User')) {
             throw new InvalidArgumentException('Missing token information');
         }
         try {
-            // $response = $this->api->delete(sprintf('/portal/developers/key/%s/%s/%s',
-            //     $this->policy,
-            //     $this->hash,
-            //     $this->user->get_tyk_id()
-            //     ));
-            // if (is_object($response) && isset($response->Status) && $response->Status == 'OK') {
-            //     PC::debug($this);
 
-            //     if (Tyk_Dev_Portal::is_hybrid()) {
-                    
-                    
-                    $apiManager = new Tyk_API_Manager;
-                    $apis = $apiManager->available_apis();
-                   
-                    PC::debug(sprintf('/apis/%s/keys/%s', 
-                        $apis[3]->api_definition->api_id,
-                        $this->key                       
-                    ));
-                    PC::debug($this->api);
-                    $response = $this->api->delete(sprintf('/apis/%s/keys/%s', 
-                        $apis[3]->api_definition->api_id,
-                        $this->key
-                    ));
-                    if (is_object($response) && isset($response->Status) && $response->Status == 'OK') {
-                        PC::debug($response, 'returning');
-                        return true;
-                    }
-                    else {
-                        PC::debug($response, 'hmmm');
-                        throw new Exception('Received invalid response from Tyk Api');
-                    }
+            $apiManager = new Tyk_API_Manager;
+            $apis       = $apiManager->available_apis();
 
-                    //return true;
-                //}
-                    //PC::debug('hdfdfdfdf');
-                //return true;
-            // }
-            // else {
-            //     PC::debug($response, 'invalid');
-            //     throw new Exception('Received invalid response from API');
-            // }
-        }
-        catch (Exception $e) {
-            PC::debug($e, 'exc');
+            // delete key from tyk redis central db
+            $response = $this->api->delete(sprintf('/apis/%s/keys/%s',
+                $apis[3]->api_definition->api_id,
+                $this->key
+            ));
+
+            if (is_object($response) && isset($response->Status) && $response->Status == 'OK') {
+
+                // delete key from gateway workers
+                $response = $this->gateway->delete(sprintf('/keys/%s?hashed=1', $this->hash));
+                if (is_object($response) && isset($response->status) && $response->status == 'ok') {
+                    return true;
+                } else {
+                    throw new Exception('Received invalid response from Gateway');
+                }
+
+            }
+
+        } catch (Exception $e) {
+            //PC::debug($e, 'exc');
             throw new UnexpectedValueException($e->getMessage());
         }
     }
-
 
     /**
      * Get usage quota of this token
@@ -347,58 +333,56 @@ class Tyk_Token
      *
      * @return object Usage quota
      */
-    public function get_usage_quota() {
+    public function get_usage_quota()
+    {
         if (!is_string($this->key)) {
             throw new InvalidArgumentException('Missing token key');
         }
 
         try {
-	    //$obj = (object)null;
+            //$obj = (object)null;
             //$obj -> gateway = $this->gateway;
-	    //$obj -> request = sprintf('/keys/%s', $this->key);
+            //$obj -> request = sprintf('/keys/%s', $this->key);
             //$this -> var_error_log( $obj );
             /**
-			 * Hybrid Tyk
-			 * Get usage quota from gateways, as this info isn't synced back to cloud
-			 */
-			if (Tyk_Dev_Portal::is_hybrid()) {
-				$response = $this->gateway->get(sprintf('/keys/%s', $this->key));
+             * Hybrid Tyk
+             * Get usage quota from gateways, as this info isn't synced back to cloud
+             */
+            if (Tyk_Dev_Portal::is_hybrid()) {
+                $response = $this->gateway->get(sprintf('/keys/%s', $this->key));
 
-				if (is_object($response) && isset($response->quota_remaining)) {
-					return (object) array(
-						'quota_remaining' => $response->quota_remaining,
-						'quota_max' => $response->quota_max
-						);
-				}
-				else {
-					throw new Exception('Received invalid response from Gateway');
-				}
-			}
-			/**
-			 * Cloud and on-premise Tyk
-			 * Get usage quota from API
-			 */
-			else {
-				// first: we need an api id on which to request the tokens
-				// sounds weird I know, here's the explanation: https://community.tyk.io/t/several-questions/1041/3
-				$apiManager = new Tyk_API_Manager;
-				$apis = $apiManager->available_apis();
-				if (is_array($apis)) {
-					$firstApi = array_shift($apis);
-					$response = $this->api->get(sprintf('/apis/%s/keys/%s',
-						$firstApi->api_definition->api_id,
-						$this->key
-					));
-				}
-				if (is_object($response) && isset($response->data)) {
-					return $response->data;
-				}
-				else {
-					throw new Exception('Received invalid response from API');
-				}
-			}
-        }
-        catch (Exception $e) {
+                if (is_object($response) && isset($response->quota_remaining)) {
+                    return (object) array(
+                        'quota_remaining' => $response->quota_remaining,
+                        'quota_max'       => $response->quota_max,
+                    );
+                } else {
+                    throw new Exception('Received invalid response from Gateway');
+                }
+            }
+            /**
+             * Cloud and on-premise Tyk
+             * Get usage quota from API
+             */
+            else {
+                // first: we need an api id on which to request the tokens
+                // sounds weird I know, here's the explanation: https://community.tyk.io/t/several-questions/1041/3
+                $apiManager = new Tyk_API_Manager;
+                $apis       = $apiManager->available_apis();
+                if (is_array($apis)) {
+                    $firstApi = array_shift($apis);
+                    $response = $this->api->get(sprintf('/apis/%s/keys/%s',
+                        $firstApi->api_definition->api_id,
+                        $this->key
+                    ));
+                }
+                if (is_object($response) && isset($response->data)) {
+                    return $response->data;
+                } else {
+                    throw new Exception('Received invalid response from API');
+                }
+            }
+        } catch (Exception $e) {
             throw new UnexpectedValueException($e->getMessage());
         }
     }
@@ -413,19 +397,20 @@ class Tyk_Token
      * @param string $to_date To this date
      * @return object Usage data
      */
-    public function get_usage($from_date = null, $to_date = null) {
+    public function get_usage($from_date = null, $to_date = null)
+    {
         if (!is_string($this->hash)) {
             throw new InvalidArgumentException('Missing token hash');
         }
 
         // use from_date or today-1 month
         $from = is_null($from_date)
-            ? strtotime('-1 week')
-            : strtotime($from_date);
+        ? strtotime('-1 week')
+        : strtotime($from_date);
         // use to_date or <now>
         $to = is_null($to_date)
-            ? time()
-            : strtotime($to_date);
+        ? time()
+        : strtotime($to_date);
 
         try {
             $response = $this->api->get(sprintf('/activity/keys/%s/%s/%s',
@@ -434,17 +419,15 @@ class Tyk_Token
                 date('j/n/Y', $to)
             ), array(
                 'res' => 'day',
-                'p' => -1
+                'p'   => -1,
             ));
 
             if (is_object($response) && property_exists($response, 'data')) {
                 return $response->data;
-            }
-            else {
+            } else {
                 throw new Exception('Received invalid response from API');
             }
-        }
-        catch (Exception $e) {
+        } catch (Exception $e) {
             throw new UnexpectedValueException($e->getMessage());
         }
     }
