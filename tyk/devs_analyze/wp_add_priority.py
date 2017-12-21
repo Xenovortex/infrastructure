80cbe1605997a50cda52d2bdff3270840228ef5d
Nils <nils.nolde@gmail.com>
2017-12-21 02:44:07 +0000

    set the host to WP production DB

--- tyk/devs_analyze/wp_add_priority.py
+++ tyk/devs_analyze/wp_add_priority.py
@@ -1,8 +1,11 @@
 
 # coding: utf-8
 
-# In[1]:
+"""
+First: Update data/*.json
 
+This will then insert 'priority' values for user_id's which don't exist yet
+"""
 
 import os, json, webbrowser
 import pandas as pd
@@ -15,9 +18,9 @@ import numpy as np
 # In[2]:
 
 
-with open('users_wp.json', 'r') as f:
+with open('data/users_wp.json', 'r') as f:
     users_wp = json.load(f)
-with open ('users_tyk.json', 'r') as f:
+with open ('data/users_tyk.json', 'r') as f:
     users_tyk = json.load(f)
 
 
@@ -51,15 +54,14 @@ print result.head()
 # In[23]:
 
 
-conn = mysql.connect(host='172.19.0.3', user='root', passwd='admin', db='wordpress')
+conn = mysql.connect(host='172.18.0.2', user='root', passwd='admin', db='wordpress')
 cur = conn.cursor()
 sql_insert_row = "INSERT INTO wp_usermeta (user_id, meta_key) SELECT * FROM (SELECT %s, 'priority') AS tmp WHERE NOT EXISTS (SELECT * from wp_usermeta where user_id = %s and meta_key = 'priority' ) LIMIT 1;"
 sql_update_row = "UPDATE wp_usermeta SET meta_value = %s WHERE meta_key = 'priority' AND user_id = %s"
 for idx, typ in zip(result_ids, result_types):
-    cur.execute(SQL, (idx, idx))
+    cur.execute(sql_insert_row, (idx, idx))
     cur.execute(sql_update_row, (typ, idx))
-    
+
 cur.close()
 conn.commit()
 conn.close()
-
