9cddff151e1b484323480f9c6c4c32a15fc3bf5e
nilsnolde <nils.nolde@gmail.com>
2018-01-21 15:12:54 +0100

    send report mail

--- tyk/manage_users/delete_users_without_apikey.py
+++ tyk/manage_users/delete_users_without_apikey.py
@@ -3,6 +3,7 @@ import json
 import pandas as pd
 
 import infrastructure_py.databases as db
+import infrastructure_py.mail as mail
 #import MySQLdb as mysql
 
 """CAUTION: Will delete users from WordPress and Tyk.
@@ -15,11 +16,12 @@ KNOW WHAT YOU'RE DOING!
 
 def parseJSON():    
     """Parse JSON"""
-    return json.load(open('users_without_api_keys.json', 'r'))
+    return json.load(open('data/users_without_api_keys.json', 'r'))
     
 
 def validate(cached_users):
     """validate with Tyk if anything changed"""
+    tyk = db.Tyk()
     actual_devs = pd.DataFrame(tyk.getDevs()).set_index('_id')
     actual_devs = actual_devs[actual_devs['api_keys'] == {}]
     cached_devs = pd.DataFrame.from_dict(cached_users, orient='index')
@@ -29,24 +31,51 @@ def validate(cached_users):
 
 def deleteFromTyk(delete_devs):
     """delete leftover records from Tyk"""
+    tyk = db.Tyk()
     for tyk_key, email in delete_devs.iterrows():
         tyk.deleteDev(tyk_key)
-        
+    return
+    
 
 def deleteFromWP(delete_devs):
+    wp = db.WP()
     """delete leftover records from WP"""
-    pass
-#    for _, email in delete_devs.iterrows():
+    ids = wp.getWPidsByEmail(delete_devs['email'].tolist())
+    try:
+        wp.deleteUserByWPids(ids)
+        wp.commit()
+    except:
+        raise
+    finally:
+        wp.close()
+    
+    return
+
+
+def sendMailToORS(deleted_number):
+    """Send mail to status@ors.org upon completion of script"""
+    mailer = mail.Mailer()
+    cont = ("Hi Team,\n"
+           "{} developers were deleted from Tyk and WP.").format(deleted_number)
+           
+    mailer.sendText(subject="{} users deleted".format(deleted_number),
+                    source='CRM ORS <crm@openrouteservice.org>',
+                    to=['nilsnolde@gmail.com'],
+                    content=cont)
+    
+    return
     
     
-if __name__== '__main__':   
-    """Set up DB accesses"""
-    tyk = db.Tyk()
-#    wp = db.WP()
-    
+if __name__== '__main__':       
     cached_users = parseJSON()
     
+    test_mail = ['nils.nolde@zalando.de']
+    
     delete_devs = validate(cached_users)
+    
+    deleteFromWP(delete_devs)
+    
+    sendMailToORS(len(delete_devs))
     
 #    deleteFromTyk(delete_devs)
 #    
